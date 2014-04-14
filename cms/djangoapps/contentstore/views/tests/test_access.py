89ff2e4c4e5c934e9500acceaa6f44e55dccb384
Nimisha Asthagiri <nasthagiri@edx.org>
2014-04-13 23:57:25 -0400

    fixup! Make Studio support opaque-keys

--- cms/djangoapps/contentstore/views/tests/test_access.py
+++ cms/djangoapps/contentstore/views/tests/test_access.py
@@ -21,25 +21,24 @@ class RolesTest(TestCase):
         self.global_admin = AdminFactory()
         self.instructor = User.objects.create_user('testinstructor', 'testinstructor+courses@edx.org', 'foo')
         self.staff = User.objects.create_user('teststaff', 'teststaff+courses@edx.org', 'foo')
-        self.location = Location('mitX', '101', 'course', 'test')
-        self.locator = CourseLocator.from_string('edx://mitX+101.test')
+        self.course_key = CourseLocator.from_string('edx:mitX+101.test')
 
     def test_get_user_role_instructor(self):
         """
         Verifies if user is instructor.
         """
-        add_users(self.global_admin, CourseInstructorRole(self.location.course_key), self.instructor)
+        add_users(self.global_admin, CourseInstructorRole(self.course_key), self.instructor)
         self.assertEqual(
             'instructor',
-            get_user_role(self.instructor, self.location.course_key)
+            get_user_role(self.instructor, self.course_key)
         )
 
     def test_get_user_role_staff(self):
         """
         Verifies if user is staff.
         """
-        add_users(self.global_admin, CourseStaffRole(self.location.course_key), self.staff)
+        add_users(self.global_admin, CourseStaffRole(self.course_key), self.staff)
         self.assertEqual(
             'staff',
-            get_user_role(self.staff, self.location.course_key)
+            get_user_role(self.staff, self.course_key)
         )
