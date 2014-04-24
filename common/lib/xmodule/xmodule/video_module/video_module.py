2dce35b9eb0eb81a2c7b670a624044d64ae4fe7b
polesye <s2pak.anton@gmail.com>
2014-04-24 15:07:19 +0300

    Fix grade_url method.

--- common/lib/xmodule/xmodule/video_module/video_module.py
+++ common/lib/xmodule/xmodule/video_module/video_module.py
@@ -173,7 +173,7 @@ class VideoModule(VideoFields, VideoStudentViewHandlers, XModule):
             'transcript_languages': json.dumps(sorted_languages),
             'transcript_translation_url': self.runtime.handler_url(self, 'transcript', 'translation').rstrip('/?'),
             'transcript_available_translations_url': self.runtime.handler_url(self, 'transcript', 'available_translations').rstrip('/?'),
-            'grade_url': self.grade_url
+            'grade_url': grade_url(self)
         })
 
 
